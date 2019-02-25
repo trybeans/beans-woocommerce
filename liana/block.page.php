1e6adedd307e10cbe83403bef1f5ccc02ff46f2d
yan <yan@trybeans.com>
2019-02-25 15:38:10 +0100

    - update rewards program page

--- liana/block.page.php
+++ liana/block.page.php
@@ -10,12 +10,13 @@ use BeansWoo\Helper;
         for (var o = 0; o < i.length; o++) {a(i[o])}e.Beans3 = r
     })(window);
 </script>
+<div class="container main content main-wrapper wrapper wrapper--margins">
+  <div id="liana-page">
+    <!-- DO NOT TOUCH -->
+  </div>
+</div>
 
-<script type="text/javascript" src="https://npmcdn.com/react@15.3.0/dist/react.js"></script>
-<script type="text/javascript" src="https://npmcdn.com/react-dom@15.3.0/dist/react-dom.js"></script>
-<script type="text/javascript" src="https://s3-us-west-2.amazonaws.com/bnsre/static-v3/liana/lib/3.0/js/beans-loyalty-page.js"></script>
-<link rel="stylesheet" type="text/css" href="https://s3-us-west-2.amazonaws.com/bnsre/static-v3/liana/lib/3.0/css/beans-loyalty-page.css">
-<!--<div class="container main content main-wrapper wrapper wrapper--margins">-->
+<div class="container main content main-wrapper wrapper wrapper--margins">
   <div class="rewards-page">
     <div class="rewards-page-grid">
       <div class="rewards-page-grid-item">
@@ -25,9 +26,16 @@ use BeansWoo\Helper;
       </div>
     </div>
   </div>
-<!--</div>-->
+</div>
+
+
+<script type="text/javascript" src="https://npmcdn.com/react@15.3.0/dist/react.js"></script>
+<script type="text/javascript" src="https://npmcdn.com/react-dom@15.3.0/dist/react-dom.js"></script>
+<script type="text/javascript" src="https://s3-us-west-2.amazonaws.com/bnsre/static-v3/liana/lib/3.0/js/beans-loyalty-page.js"></script>
+<link type="text/css" rel="stylesheet" href="https://s3-us-west-2.amazonaws.com/bnsre/static-v3/liana/lib/3.0/css/beans-loyalty-page.css">
+
 <div style="margin: 50px auto 20px; max-width: 800px; text-align: center">
   <p style="text-align: center">
-    This <a href="https://<?php echo Helper::getDomain('WWW');?>/">loyalty and rewards program</a> is powered by Beans
+    This <a href="https://www.trybeans.com/">rewards program</a> is powered by Beans
   </p>
 </div>
