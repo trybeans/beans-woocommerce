704235070e981cd6e6c4676183d4ff5e9f83b888
Corentin ALLOHOUMBO <corentin@trybeans.com>
2019-07-23 14:17:26 -0400

    hide button

--- liana/block.php
+++ liana/block.php
@@ -10,12 +10,12 @@ class Block {
         add_filter('wp_enqueue_scripts',                             array(__CLASS__, 'enqueue_scripts'), 10, 1);
         add_filter('wp_head',                                        array(__CLASS__, 'render_head'),     10, 1);
         add_filter('woocommerce_after_cart_table',                   array(__CLASS__, 'render_cart'),     10, 1);
-        if (is_user_logged_in()){
-            add_filter('woocommerce_before_checkout_form',               array(__CLASS__, 'render_cart'),     15, 1);
-        }
         add_filter('woocommerce_register_form_start',                array(__CLASS__, 'render_register'), 15, 1);
         add_filter('wp_footer',                                      array(__CLASS__, 'render_init'),     10, 1);
         add_filter('the_content',                                    array(__CLASS__, 'render_page'),     10, 1);
+        if (is_user_logged_in() && isset($_SESSION['liana_account']) ){
+            add_filter('woocommerce_before_checkout_form',               array(__CLASS__, 'render_cart'),     15, 1);
+        }
     }
 
     public static function enqueue_scripts(){
