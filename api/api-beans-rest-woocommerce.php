88b4173d54178dc86d62ae886e4b38761658a192
Corentin ALLOHOUMBO <corentin@trybeans.com>
2019-10-17 11:24:35 -0400

    add shop page uri into system status rest response

--- api/api-beans-rest-woocommerce.php
+++ api/api-beans-rest-woocommerce.php
@@ -12,6 +12,8 @@ class BeansRestWoocommerce
         # add_filter('woocommerce_rest_prepare_coupon_object',    array(__CLASS__, 'add_beans_app_activated'), 90, 1);
         add_filter('woocommerce_rest_prepare_shop_order_object',  array(__CLASS__, 'add_beans_app_activated'), 90, 1);
         add_filter('woocommerce_rest_prepare_product_object',     array(__CLASS__, 'add_beans_app_activated'), 90, 1);
+
+        add_filter('woocommerce_rest_prepare_system_status',      array(__CLASS__, 'add_site_pages_infos'),    20, 1);
     }
 
     public static function add_beans_app_activated($response){
@@ -23,8 +25,28 @@ class BeansRestWoocommerce
                 $installed_apps[] = $app_name;
             }
         }
-        $response->data["beans_app"] = $installed_apps;
+        $response->data["beans_apps"] = $installed_apps;
 
+        return $response;
+    }
+
+    public static function add_site_pages_infos($response){
+        $pages = [
+            "login" =>  get_permalink(get_option('woocommerce_myaccount_page_id')),
+            "cart" =>   get_permalink(get_option('woocommerce_cart_page_id')),
+            "product" => get_permalink(get_option('woocommerce_shop_page_id')),
+            "checkout" => get_permalink(get_option('woocommerce_checkout_page_id')),
+            "reward" => get_permalink(Helper::getConfig('liana_page')),
+        ];
+
+        foreach ($pages as &$link){
+            $link = str_replace(home_url(), '', $link);
+        }
+
+        if (!isset($response->data['pages'])){
+            $response->data['pages'] = [];
+        }
+        $response->data['pages']['beans'] = $pages;
         return $response;
     }
 }
