f780734938d9b7660a7495834f7344649c7f4a3f
Corentin ALLOHOUMBO <corentin@trybeans.com>
2019-10-18 11:14:39 -0400

    refactor code

--- api/api-beans-rest-woocommerce.php
+++ api/api-beans-rest-woocommerce.php
@@ -8,12 +8,15 @@ use BeansWoo\Helper;
 class BeansRestWoocommerce
 {
     public static function init(){
-        add_filter('woocommerce_rest_prepare_customer',           array(__CLASS__, 'add_beans_app_activated'), 90, 1);
-        # add_filter('woocommerce_rest_prepare_coupon_object',    array(__CLASS__, 'add_beans_app_activated'), 90, 1);
-        add_filter('woocommerce_rest_prepare_shop_order_object',  array(__CLASS__, 'add_beans_app_activated'), 90, 1);
-        add_filter('woocommerce_rest_prepare_product_object',     array(__CLASS__, 'add_beans_app_activated'), 90, 1);
+        if (Helper::isSetup()){
 
-        add_filter('woocommerce_rest_prepare_system_status',      array(__CLASS__, 'add_site_pages_infos'),    20, 1);
+            add_filter('woocommerce_rest_prepare_customer',           array(__CLASS__, 'add_beans_app_activated'), 90, 1);
+            # add_filter('woocommerce_rest_prepare_coupon_object',    array(__CLASS__, 'add_beans_app_activated'), 90, 1);
+            add_filter('woocommerce_rest_prepare_shop_order_object',  array(__CLASS__, 'add_beans_app_activated'), 90, 1);
+            add_filter('woocommerce_rest_prepare_product_object',     array(__CLASS__, 'add_beans_app_activated'), 90, 1);
+
+            add_filter('woocommerce_rest_prepare_system_status',      array(__CLASS__, 'add_site_pages_infos'),    90, 2);
+        }
     }
 
     public static function add_beans_app_activated($response){
@@ -30,13 +33,24 @@ class BeansRestWoocommerce
         return $response;
     }
 
-    public static function add_site_pages_infos($response){
+    public static function add_site_pages_infos($response, $system_status){
         $pages = [
-            "login" =>  get_permalink(get_option('woocommerce_myaccount_page_id')),
-            "cart" =>   get_permalink(get_option('woocommerce_cart_page_id')),
-            "product" => get_permalink(get_option('woocommerce_shop_page_id')),
-            "checkout" => get_permalink(get_option('woocommerce_checkout_page_id')),
-            "reward" => get_permalink(Helper::getConfig('liana_page')),
+            get_option('woocommerce_myaccount_page_id') =>  [
+                'path' => get_permalink(get_option('woocommerce_myaccount_page_id')),
+                'type' => 'login'
+            ],
+            get_option('woocommerce_cart_page_id') =>   [
+                'path' => get_permalink(get_option('woocommerce_cart_page_id')),
+                'type' => 'cart',
+            ],
+            get_option('woocommerce_shop_page_id') => [
+                'path' => get_permalink(get_option('woocommerce_shop_page_id')),
+                'type' => 'product',
+            ],
+            get_option('woocommerce_checkout_page_id') => [
+                    'path' => get_permalink(get_option('woocommerce_checkout_page_id')),
+                    'type' => 'checkout'
+            ],
         ];
 
         foreach ($pages as &$link){
@@ -46,7 +60,59 @@ class BeansRestWoocommerce
         if (!isset($response->data['pages'])){
             $response->data['pages'] = [];
         }
-        $response->data['pages']['beans'] = $pages;
+
+        foreach ($response->data['pages'] as &$page){
+            if(isset($pages[$page['page_id']])){
+                $page = array_merge($page, $pages[$page['page_id']]);
+            }
+        }
+
+        $response->data['pages'] = array_merge($response->data['pages'], self::get_beans_pages());
+
         return $response;
+    }
+
+    public static function get_beans_pages(){
+        $pages_output = [];
+
+        foreach (Helper::getPages() as $app_name => $values){
+
+            if(! Helper::isSetupApp($app_name)) {
+                continue;
+            }
+
+            $page_id = $values['page_id'];
+            $page_exists        = false;
+            $page_visible       = false;
+
+            if ( $page_id ) {
+                $page_set = true;
+            }
+            else{
+                continue;
+            }
+
+            if ( get_post( $page_id ) ) {
+                $page_exists = true;
+            }
+            if ( 'publish' === get_post_status( $page_id ) ) {
+                $page_visible = true;
+            }
+
+            $pages_output[] = array(
+                'page_name'          => $values['page_name'],
+                'page_id'            => $page_id,
+                'page_set'           => $page_set,
+                'page_exists'        => $page_exists,
+                'page_visible'       => $page_visible,
+                'shortcode'          => $values['shortcode'],
+                'shortcode_required' => True,
+                'shortcode_present'  => True,
+                'type' => $values['type'],
+                'path' => str_replace(home_url(), '', get_permalink(Helper::getConfig('liana_page'))),
+            );
+        }
+
+        return $pages_output;
     }
 }
