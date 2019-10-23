1be0c85dd8ab0e826476840094eaab708caec73f
Corentin ALLOHOUMBO <corentin@trybeans.com>
2019-10-23 11:10:33 -0400

    add uninstall hook

--- beans-woo.php
+++ beans-woo.php
@@ -117,3 +117,21 @@ function wc_beans_plugins_deactivate(){
 register_deactivation_hook(__FILE__, function (){
     wc_beans_plugins_deactivate();
 });
+
+register_uninstall_hook(__FILE__, function(){
+    $args = array(
+        'status' => 'uninstalled',
+        'shop_url' => home_url(),
+    );
+
+    WC_Beans::send_status($args);
+
+    // if uninstall not called from WordPress exit
+    if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+        exit;
+    }
+
+    delete_option('beans-config-3');
+    delete_option('beans-liana-display-redemption-checkout');
+
+});
