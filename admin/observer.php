45b06b9bd6cf35aad5791d180a6ee38c4e639fbb
LionelD <dah-lionel@hotmail.com>
2020-03-31 11:33:09 +0100

    - remove old synchronise method - minor re-structuring of assets/img folder

--- admin/observer.php
+++ admin/observer.php
@@ -42,8 +42,7 @@ class Observer
 
     public static function admin_style()
     {
-        wp_enqueue_style('admin-styles', plugins_url('assets/css/beans-admin.css',
-            BEANS_PLUGIN_FILENAME));
+        wp_enqueue_style('admin-styles', plugins_url('assets/css/beans-admin.css', BEANS_PLUGIN_FILENAME));
     }
 
     public static function setting_options()
@@ -107,55 +106,4 @@ class Observer
         }
     }
 
-
-    /**
-     * private static function synchronise(){
-     *
-     * $estimated_account = 0;
-     * $contact_data = array();
-     *
-     * $users_count = count_users();
-     * if(isset($users_count['avail_roles']['customer'])){
-     * $estimated_account = $users_count['avail_roles']['customer'];
-     * }
-     *
-     * if (current_user_can('manage_woocommerce')){
-     * $admin = wp_get_current_user();
-     * $contact_data = array(
-     * 'email' => $admin->user_email,
-     * 'last_name' => $admin->user_lastname,
-     * 'first_name' => $admin->user_firstname,
-     * );
-     * }
-     *
-     * $country_code = get_option('woocommerce_default_country');
-     * if($country_code && strpos($country_code, ':') !== false){
-     * try {
-     * $country_parts = explode( ':', $country_code );
-     * $country_code = $country_parts[0];
-     * }catch(\Exception $e){}
-     * }
-     *
-     * $data = array(
-     * 'website' => get_site_url(),
-     * 'currency' => strtoupper(get_woocommerce_currency()),
-     * 'company_name' => get_bloginfo('name'),
-     * 'store_name' => get_bloginfo('name'),
-     * 'country_code' => $country_code,
-     * 'contact' => $contact_data,
-     * 'estimated_account' => $estimated_account,
-     * 'php_version' => phpversion(),
-     * 'plugin_version' => BEANS_VERSION,
-     * );
-     *
-     * try{
-     * $response = Helper::API()->post('hook/integrations/woocommerce/synchronise', $data);
-     * if(isset($response['result'])) return $response['result'];
-     * }catch (\Exception $e) {
-     * Helper::log('Unable to sync: '.$e->getMessage());
-     * }
-     *
-     * return false;
-     * }
-     **/
 }
