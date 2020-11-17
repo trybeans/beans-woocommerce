0ae575fbb99875e097dabf9fbfb6076f78474f6e
yandah <yan@trybeans.com>
2020-11-17 18:02:56 +0100

    - fix access to display

--- front/liana/init.php
+++ front/liana/init.php
@@ -11,12 +11,11 @@ define( 'BEANS_LIANA_COUPON_UID', 'redeem_points' );
 
 
 class Main {
-    public static $display;
 
     public static function init() {
 
-        self::$display = Helper::getBeansObject('liana', 'display');
-        if ( empty( self::$display ) || ! self::$display['is_active'] || !Helper::isSetupApp('liana')) {
+        $display = Helper::getBeansObject('liana', 'display');
+        if ( empty( $display ) || ! self::$display['is_active'] || !Helper::isSetupApp('liana')) {
             return;
         }
 
