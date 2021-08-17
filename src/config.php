25094059fa120cd89acbd78ce6bf6e560564f35c
yandah <yan@trybeans.com>
2021-08-17 13:01:12 +0100

    update config file

--- src/config.php
+++ src/config.php
@@ -5,9 +5,12 @@ defined('ABSPATH') or exit;
 
 if (!defined('BEANS_PLUGIN_VERSION')) {
     define('BEANS_PLUGIN_VERSION', '3.3.0');
-    define('BEANS_PLUGIN_FILENAME', plugin_basename(__FILE__));
-    define('BEANS_PLUGIN_PATH', plugin_dir_path(__FILE__));
 }
 
+if (!defined('BEANS_PLUGIN_FILENAME')) {
+    define('BEANS_PLUGIN_FILENAME', plugin_basename(__FILE__));
+}
 
-/* Server */
+if (!defined('BEANS_PLUGIN_FILENAME')) {
+    define('BEANS_PLUGIN_PATH', plugin_dir_path(__FILE__));
+}
