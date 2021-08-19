9914df5444b6ac000a09de6351c6cc2277745316
yandah <yan@trybeans.com>
2021-08-19 14:41:02 +0100

    delete admin observer

--- src/admin/Router.php
+++ src/admin/Router.php
@@ -9,6 +9,7 @@ defined('ABSPATH') or die;
 class Router
 {
     const MENU_SLUG = 'beans-woo'; // private
+    const NOTICE_KEY = 'beans_ultimate_notice_dismissed'; // private
     const TAB_INSPECT = 'inspect'; // public
     const TAB_CONNECT = 'connect'; // public
     const TAB_SETTINGS = 'settings'; // public
@@ -18,6 +19,9 @@ class Router
     public static function init()
     {
         add_action('admin_menu', array(__CLASS__, 'registerRoutes'));
+        add_action('admin_notices', array(__CLASS__, 'displayAdminNotice'));
+
+        add_filter("plugin_action_links_" . BEANS_PLUGIN_FILENAME, array(__CLASS__, 'addPluginActionLinks' ), 10, 1);
     }
 
     public static function registerRoutes()
@@ -44,7 +48,7 @@ class Router
         }
     }
 
-    private static function renderNotices()
+    private static function renderPageNotices()
     {
 
         if (self::$_messages_error) {
@@ -79,6 +83,7 @@ class Router
 
     public static function renderAdminPage()
     {
+
         $tab = null;
         if (isset($_GET['tab'])) {
             $tab = $_GET['tab'];
@@ -96,7 +101,15 @@ class Router
             }
         }
 
-        self::renderNotices();
+        if (isset($_GET[self::NOTICE_KEY])) {
+            if (self::discardAdminNotice()) {
+                return wp_safe_redirect($_SERVER['HTTP_REFERER']);
+            }
+        }
+
+        self::loadPageStyle();
+
+        self::renderPageNotices();
 
         /* For authenticated retailers */
 
@@ -118,5 +131,59 @@ class Router
         }
 
         return wp_redirect(self::getTabURL(self::TAB_INSPECT));
+    }
+
+    public static function loadPageStyle()
+    {
+        wp_enqueue_style(
+            'admin-styles',
+            Helper::getAssetURL('assets/css/beans-admin.css')
+        );
+    }
+
+    public static function addPluginActionLinks($links)
+    {
+        $row_meta = array(
+            'settings' => '<a href=' . self::getTabURL(self::TAB_SETTINGS) . '>Settings</a>',
+            'help' => '<a href="http://help.trybeans.com/" target="_blank" title="Help">Help Center</a>',
+            'support' => '<a href="mailto:hello@trybeans.com" title="Support">Contact Support</a>',
+        );
+
+        return array_merge($links, $row_meta);
+    }
+
+    public static function displayAdminNotice()
+    {
+        $user_id = get_current_user_id();
+        if (get_user_meta($user_id, self::NOTICE_KEY)) {
+            return;
+        }
+
+        if (!Helper::isSetup()) {
+            ?>
+            <div class="notice notice-error" style="margin-left: auto">
+                <div style="margin: 10px auto;">
+                Beans: <?=__("Beans Ultimate is not properly setup", 'beans-woo');?>
+                <a href="<?=self::getTabURL(self::TAB_INSPECT)?>"><?=__('Set up', 'beans-woo')?></a>
+                <a href="<?=self::getTabURL()?>&<?=self::NOTICE_KEY?>=1" 
+                    style="float:right; text-decoration: none">
+                    x
+                </a>
+                </div>
+            </div>
+            <?php
+        }
+    }
+
+    private static function discardAdminNotice()
+    {
+        $user_id = get_current_user_id();
+        add_user_meta(
+            $user_id,
+            self::NOTICE_KEY,
+            'true',
+            true
+        );
+        return true;
     }
 }
