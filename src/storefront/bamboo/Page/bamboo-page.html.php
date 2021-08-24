ef8b695493d05ee6d7d38cfe3573009b45415ba5
corentin <corentin@trellix.io>
2021-08-24 14:35:44 +0100

    - Add CSS class to the footer section of program pages

--- src/storefront/bamboo/Page/bamboo-page.html.php
+++ src/storefront/bamboo/Page/bamboo-page.html.php
@@ -8,8 +8,8 @@ defined('ABSPATH') or die;
 <div id="bamboo-referral-page">
 <!-- DO NOT TOUCH -->
 </div>
-<div style="margin: 50px auto 20px; max-width: 800px; text-align: center">
-  <p style="text-align: center">
+<div class="beans-page-footer">
+  <p>
     This
     <a href="https://<?php echo Helper::getDomain('WWW'); ?>/bamboo" target="_blank">
       referral program
--- src/storefront/liana/Page/liana-page.html.php
+++ src/storefront/liana/Page/liana-page.html.php
@@ -10,8 +10,8 @@ defined('ABSPATH') or die;
 <div id="liana-rewards-page">
 <!-- DO NOT TOUCH -->
 </div>
-<div style="margin: 50px auto 20px; max-width: 800px; text-align: center">
-  <p style="text-align: center">
+<div class ="beans-page-footer">
+  <p>
     This <a href="https://<?php echo Helper::getDomain('WWW'); ?>/liana"
             target="_blank">rewards program</a> is powered by Beans
   </p>
