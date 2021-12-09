70c7e3f32c831116686e0e09e499ea37ade7f8d4
corentin <corentin@trellix.io>
2021-12-09 11:14:12 +0100

    - Add changelog for 3.3.4

--- src/includes/Beans.php
+++ src/includes/Beans.php
@@ -33,7 +33,7 @@ class Beans
 {
     public $endpoint = 'https://api.trybeans.com/v3/';
 
-    const VERSION = '3.3.3';  // private
+    const VERSION = '3.3.4';  // private
 
     private $_secret = '';
     private $_next_page = '';
